d9908da37e8740c1492d36fcd1bf5f65401d06c0
VitaPasse <vitapasser@gmail.com>
2023-09-03 16:43:22 +0300

    Migrate to React

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.use(express.static('app'));
-
-const server = app.listen(3000, function() {
-    console.log('working on', server.address().port);
-});
